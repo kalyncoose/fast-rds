6df5c30520936b701a2b5aa7a0ed041ba4e8d8aa
Kalyn <kalyncoose@gmail.com>
2020-08-13 18:08:30 -0500

    Added license information

--- functions/create_with_config.py
+++ functions/create_with_config.py
@@ -6,7 +6,20 @@
  / /   /_/  \__,_/____/\__/     /_/   \__,_/____/  _/ /   
 /__/   https://github.com/kalyncoose/fast-rds     /__/    
 
-Provided by Apache 2.0 License
+Copyright 2020 Kalyn Coose
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
 See requirements.txt for other packages and their licenses.
 """
 
